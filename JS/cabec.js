f6664ee159263e155d492582f8b1b6d1a03cb6da
Emerson Costa <syphonemerson@yahoo.com.br>
2020-06-09 10:58:07 -0300

    Melhorias
    - Index finalizado
    - Footer finalizado com JS para ser usado nas outras páginas
    - Cabeçalho iniciado em JS para ser usado nas outras páginas

--- JS/cabec.js
+++ JS/cabec.js
@@ -1,5 +1,14 @@
 function cabec() 
 {
-    const html = document.getElementById('html');
+    // Adicionado informções do head e referencias de CSS e JS
+    const head = document.getElementById('head');
+
+    const title = document.createElement('title');
+    title.innerHTML = "Associação Cultural Cariri (ACC)";
+    head.appendChild(title);
+
+
+    // Adicionando menu do cabeçalho
+    const start = document.getElementById('start');
 
 }  
--- JS/footer.js
+++ JS/footer.js
@@ -1,35 +1,32 @@
 function footer() 
 {
-    const body = document.getElementById('body');
+    const start = document.getElementById('start');
 
     const footer = document.createElement('footer');
-    body.appendChild(footer);
+    footer.className="col-12 mt-5";
+    start.appendChild(footer);
 
     const div1 = document.createElement('div');
-    div1.className = 'Footer'
+    div1.className = 'row'
     footer.appendChild(div1);
 
     const div2 = document.createElement('div');
-    div2.className='mapa_footer';
+    div2.className='col-xs-12 col-md-6 w-100 d-flex justify-content-center mt-2 mb-3';
     div1.appendChild(div2)
 
     const iframe = document.createElement("iframe");
     iframe.setAttribute("src", "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.0977166211906!2d-46.67624318528077!3d-23.743894584592493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce4f5769971e65%3A0xf317438b51f25d9e!2sR.+Ver.+Jos%C3%A9+Gomes+de+Mor%C3%A3es+Neto%2C+60+-+Parque+Res.+Cocaia%2C+S%C3%A3o+Paulo+-+SP%2C+04849-030!5e0!3m2!1spt-BR!2sbr!4v1557706639363!5m2!1spt-BR!2sbr");
     iframe.width = '400px';
-    iframe.height = '400px';
+    iframe.height = '200px';
     iframe.allowFullscreen=true;
     div2.appendChild(iframe);
 
     const div3 = document.createElement ('div');
-    div3.id='margem_footer';
+    div3.className="col-xs-12 col-md-6 mt-4 text-center";
     
     const endereco = document.createElement('p');
+    endereco.className="font-weight-bold";
     endereco.innerHTML="Localização: <br> Rua Vereador José Gomes de Moares Neto,60 - Parque Residencial Cocaia<br> 04849-030 - Sao Paulo - SP<br>";
-    
-    const icone = document.createElement('i');
-    icone.className="fa fa-facebook-official";
-    icone.style="font-size:36px";
-
     const url = document.createElement('a');
     url.title = "Siga no Facebook";
     url.href = "https://facebook.com/associacaocc/";
@@ -37,11 +34,10 @@ function footer()
     const faceimg = document.createElement('img');
     faceimg.setAttribute('id', "facebook" );
     faceimg.setAttribute('src', "Imagens/facebook.png" );
-    faceimg.setAttribute('class', "img-responsive");
+    faceimg.setAttribute('class', "mt-2");
     faceimg.setAttribute('alt', "facebook");
 
     div3.appendChild(endereco);
-    endereco.appendChild(icone);
     endereco.appendChild(url);
     url.appendChild(faceimg);
     div1.appendChild(div3);
